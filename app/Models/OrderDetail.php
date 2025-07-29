97e5688052aca0e738e1c3d3c6a9ab03dfc12322
ThuThao03-ht <thanhthaotp02019gmail.com>
2025-07-30 00:38:04 +0700

    add mối quan hệ các bảng

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -6,9 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'price',
+        'total', // thêm dòng này
+    ];
+
     public function order(){
         return $this->belongsTo(Order::class, 'order_id');
-    } 
+    }
+
     public function product(){
         return $this->belongsTo(Product::class, 'product_id');
     }
