d8084849f26bada0a5fffe2449118e4ba2e8b1e6
ThuThao03-ht <thanhthaotp02019gmail.com>
2025-07-30 00:37:11 +0700

    add mối quan hệ giữ các bảng và đặt tên các cột để lưu dữ liệu

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,10 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    public function user(){
+    protected $fillable = [
+        'customerName', // phải giống tên cột trong DB
+        'email',
+        'address',
+        'phone',
+        'gender',
+        'birthDay'
+    ];
+
+    public function user()
+    {
         return $this->hasMany(User::class);
     }
-    public function order(){
-        return $this-> hasMany(Order::class);
+
+    public function order()
+    {
+        return $this->hasMany(Order::class);
     }
 }
