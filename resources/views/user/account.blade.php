62cf06bda5fb6292f7516df9ccdc3317918a3fa8
ThuThao03-ht <thanhthaotp02019gmail.com>
2025-07-31 02:24:46 +0700

    add view

---
+++ resources/views/user/account.blade.php
@@ -0,0 +1,46 @@
+<div class="modal fade" id="accountModal" tabindex="-1" aria-labelledby="accountModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="user-info">
+                    <img src="{{ asset('images/avatar/user-1.png') }}" alt="Avatar" class="avatar" width="50">
+                    <div>
+                        <h5 class="modal-title" id="accountModalLabel">{{ Auth::user()->name }}</h5>
+                        <span class="email">{{ Auth::user()->email }}</span>
+                    </div>
+                </div>
+                <button type="button" class="btn-edit" id="editButton"><i class="bi bi-pencil"></i></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Đóng"></button>
+            </div>
+
+            <div class="modal-body">
+                <div class="info-item">
+                    <label>Name</label>
+                    <span class="info-value">{{ Auth::user()->name }}</span>
+                    <input type="text" class="form-control edit-field" name="name" value="{{ Auth::user()->name }}"
+                        style="display:none;">
+                </div>
+                <div class="info-item">
+                    <label>Email account</label>
+                    <span class="info-value">{{ Auth::user()->email }}</span>
+                    <input type="email" class="form-control edit-field" name="email" value="{{ Auth::user()->email }}"
+                        style="display:none;">
+                </div>
+                <div class="info-item">
+                    <label>Quyền</label>
+                    <span class="info-value">{{ Auth::user()->utype }}</span>
+                    <input type="text" class="form-control edit-field" value="{{ Auth::user()->utype }}" disabled
+                        style="display:none;">
+                </div>
+            </div>
+
+            <div class="modal-footer" id="modalFooter" style="display:none;">
+                <form action="{{ route('admin.account.update', Auth::user()->id) }}" method="POST" id="editForm">
+                    @csrf
+                    <button type="submit" class="btn-save">Cập nhật</button>
+                </form>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+            </div>
+        </div>
+    </div>
+</div>
