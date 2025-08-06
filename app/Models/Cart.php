a704bcfdb6c509f96bc29ef785be8188abdbad9a
ThuThao03-ht <thanhthaotp02019gmail.com>
2025-08-07 02:13:07 +0700

    no

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,7 +8,7 @@ class Cart extends Model
 {
       protected $fillable = ['user_id']; // thêm fillable để firstOrCreate hoạt động
     public function user(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class,'user_id');
     }
     
     // public function cartItem(){
